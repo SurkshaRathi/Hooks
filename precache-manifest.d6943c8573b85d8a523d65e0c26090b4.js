6451b525f110ac078e19bbe1a93327197c24dce0
surksha rathi <surksha8@gmail.com>
2019-04-23 17:21:30 +0530

    Updates

---
+++ precache-manifest.d6943c8573b85d8a523d65e0c26090b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5ea3c8a60a857252e210bcd38562ae",
+    "url": "/Hooks/index.html"
+  },
+  {
+    "revision": "260ff46cb0bdc21d8ecf",
+    "url": "/Hooks/static/css/main.9cd44803.chunk.css"
+  },
+  {
+    "revision": "609637d649fbdf27206c",
+    "url": "/Hooks/static/js/2.74462cb6.chunk.js"
+  },
+  {
+    "revision": "260ff46cb0bdc21d8ecf",
+    "url": "/Hooks/static/js/main.0cb9b44c.chunk.js"
+  },
+  {
+    "revision": "91b3e98c6b5e8a1afeb6",
+    "url": "/Hooks/static/js/runtime~main.ee5444df.js"
+  }
+]);
